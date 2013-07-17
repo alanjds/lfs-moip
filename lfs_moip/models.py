e9fbf4b99854b197714f8d11af126a7fcd980ecd
Alan Justino <alan.justino@yahoo.com.br>
2013-07-17 00:13:31 -0300

    Better bill_text handling

--- lfs_moip/models.py
+++ lfs_moip/models.py
@@ -22,10 +22,16 @@ class MoipProcessor(PaymentMethodProcessor):
         else:
             raise RuntimeError()
 
-        moip = Moip('Pedido %s em %s' % ( transaction_id, self.request.build_absolute_uri('/').replace('http://', '').strip('/') ))
+        bill_text = 'Compra %s em %s' % ( transaction_id, self.request.build_absolute_uri('/').replace('http://', '').strip('/') )
+        moip = Moip(bill_text[:64]) # Docs says "max size = 64 char"
+
+        # Todo: set some person data, to save time
+        #address = dict(Logradouro='Rua xxxxx',Numero='222',Bairro='xxxx',Cidade='xxxx',Estado='xx',CEP='xxxxxx',TelefoneFixo='xxxxxxxxxx')
+        #self.moip.set_pagador(Nome='xxxx',Email='xxxxxx',Apelido='vitalbh',IdPagador='x',EnderecoCobranca=endereco)
 
         moip.set_credenciais(token=settings.MOIP_TOKEN, key=settings.MOIP_KEY)
         moip.set_ambiente(settings.get('MOIP_ENVIRONMENT', 'sandbox'))
+
         moip.set_valor(self.order.price)
         moip.set_data_vencimento(datetime.date.today().strftime('%Y-%m-%d')) # yyyy-mm-dd
 
