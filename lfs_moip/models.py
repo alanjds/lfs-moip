b21c15d161e557d3fb1538b8b15c2cc7fde86bcf
Alan Justino <alan.justino@yahoo.com.br>
2013-07-18 00:19:29 -0300

    Minimal implementation of MoIP HTML integration

--- lfs_moip/models.py
+++ lfs_moip/models.py
@@ -6,23 +6,33 @@ from django.db import models
 from django.conf import settings
 
 from lfs.plugins import PaymentMethodProcessor
+from lfs.plugins import PM_ORDER_ACCEPTED, PM_ORDER_IMMEDIATELY
+
 
 
 logger = logging.getLogger("default")
 
 
 class MoipProcessor(PaymentMethodProcessor):
+    """
+    Implements MoIP payment processor.
+    """
+
     def process(self):
         if getattr(self, 'order'):
-            transaction_id = 'ORD#%s' % self.order.pk
+            transaction_id = 'O-%s' % self.order.pk
+            price = self.order.price
         elif getattr(self, 'cart'):
-            transaction_id = 'CAR#%s' % self.cart.pk
+            transaction_id = 'C-%s' % self.cart.pk
+            price = self.cart.get_price_gross(self.request) # should it be price_net??
         else:
             raise RuntimeError()
 
-        bill_title = 'Compra %s em %s' % ( transaction_id, self.request.build_absolute_uri('/').replace('http://', '').strip('/') )
+        price = '%.0f' % (price*100) # 2 decimal houses, no decimal separator
+        bill_title = 'Compra codigo %s em %s' % ( transaction_id, self.request.build_absolute_uri('/').replace('http://', '').strip('/') )
 
-        if 'MOIP_TOKEN' in settings:
+        import ipdb; ipdb.set_trace()
+        if getattr(settings, 'MOIP_TOKEN', None):
             # API integration available!
             from moipy.moip import Moip
 
@@ -33,9 +43,9 @@ class MoipProcessor(PaymentMethodProcessor):
             #self.moip.set_pagador(Nome='xxxx',Email='xxxxxx',Apelido='vitalbh',IdPagador='x',EnderecoCobranca=endereco)
 
             moip.set_credenciais(token=settings.MOIP_TOKEN, key=settings.MOIP_KEY)
-            moip.set_ambiente(settings.get('MOIP_ENVIRONMENT', 'sandbox'))
+            moip.set_ambiente(getattr(settings, 'MOIP_ENVIRONMENT', 'sandbox'))
 
-            moip.set_valor(self.order.price)
+            moip.set_valor(price)
             moip.set_data_vencimento(datetime.date.today().strftime('%Y-%m-%d')) # yyyy-mm-dd
 
             moip.set_id_proprio(transaction_id)
@@ -55,14 +65,14 @@ class MoipProcessor(PaymentMethodProcessor):
             if accepted:
                 result['next_url'] = u'http://moip.com.br/payment?token=%s' % token
 
-        elif 'MOIP_ACCOUNT' in settings:
+        elif getattr(settings, 'MOIP_ACCOUNT', None):
             # Only HTML integration available
             from furl import furl
             f = furl('https://www.moip.com.br/PagamentoMoIP.do')
-            f['id_carteira'] = settings.MOIP_ACCOUNT
-            f['valor'] = self.order.price
-            f['nome'] = bill_title[:64] # Docs says "max size = 64 char"
-            f['id_transacao'] = transaction_id
+            f.args['id_carteira'] = settings.MOIP_ACCOUNT
+            f.args['valor'] = price
+            f.args['nome'] = bill_title[:64] # Docs says "max size = 64 char"
+            f.args['id_transacao'] = transaction_id
             result = {
                 'accepted': True,
                 'next_url': f.url,
@@ -72,7 +82,6 @@ class MoipProcessor(PaymentMethodProcessor):
             raise RuntimeError('Please configure your MoIP settings.')
 
         return result
-
 
     def get_create_order_time(self):
         return PM_ORDER_ACCEPTED
